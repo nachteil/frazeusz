d53b297aa11d414a9b40ed538bea50f24a59d1e9
Zuchens <paulina.zak1@gmail.com>
2015-12-06 10:59:24 +0100

    nlprocessor interfaces

---
+++ src/main/java/pl/edu/agh/ki/to2/nlprocessor/NLProcessor.java
@@ -0,0 +1,26 @@
+package pl.edu.agh.ki.to2.nlprocessor;
+
+
+import java.util.Set;
+
+public class NLProcessor implements IWordProvider, INLProcessor {
+
+    public void NLProcessor() {
+
+    }
+
+    public Set<String> getVariants(String word) {
+        return null;
+    }
+
+    public Set<String> getDiminutives(String word) {
+        return null;
+    }
+
+    public Set<String> getSynonyms(String word) {
+        return null;
+    }
+
+
+}
+
