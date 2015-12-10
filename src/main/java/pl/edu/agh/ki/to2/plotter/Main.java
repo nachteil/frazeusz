8de8842fb54c40bf2755ef527108e38021f0dcc0
1352168 <seyuioa@gmail.com>
2015-12-10 14:32:11 +0100

    Added timer

--- src/main/java/pl/edu/agh/ki/to2/plotter/Main.java
+++ src/main/java/pl/edu/agh/ki/to2/plotter/Main.java
@@ -1,13 +1,47 @@
 package pl.edu.agh.ki.to2.plotter;
 
+import pl.edu.agh.ki.to2.patternmatcher.models.SearchPattern;
+import pl.edu.agh.ki.to2.plotter.model.Occurrences;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Mistrz on 2015-12-10.
  */
 public class Main {
     public static void main(String [] args){
+        Map<SearchPattern, Occurrences> hashMapToCompare = new HashMap<SearchPattern, Occurrences>();
+        SearchPattern searchPattern = new SearchPattern("kot", true, true, true, true);
+        SearchPattern searchPattern2 = new SearchPattern("kot", true, true, true, true);
+        String url = "http://www.elo.pl/";
+        String url2 = "http://www.czesc.pl/";
+        List<String> sentencesList = new ArrayList<String>();
+        List<String> sentencesList2 = new ArrayList<String>();
+        sentencesList.add("slon");
+        sentencesList.add("pies");
+        sentencesList2.add("kot");
+        sentencesList2.add("mysz");
+        Occurrences occurrences = new Occurrences(url, sentencesList);
+        Occurrences occurrences2 = new Occurrences(url2, sentencesList2);
+
+
+
+        Occurrences occurrencesFull = new Occurrences(url, sentencesList);
+        occurrencesFull.add(url2, sentencesList2);
+        hashMapToCompare.put(searchPattern, occurrencesFull);
+
         Ploter ploter = new Ploter();
         ViewFrame viewFrame = new ViewFrame();
         ploter.setViewFrame(viewFrame);
         viewFrame.setPloter(ploter);
+
+
+        ploter.addMatches(searchPattern, sentencesList, url);
+        ploter.addMatches(searchPattern2, sentencesList2, url2);
+
+
     }
 }
--- src/main/java/pl/edu/agh/ki/to2/plotter/ViewFrame.java
+++ src/main/java/pl/edu/agh/ki/to2/plotter/ViewFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Sara on 2015-12-09.
@@ -13,33 +15,48 @@ public class ViewFrame extends JFrame{
     Timer timer;
     Ploter ploter;
 
-    Table tab = new Table();
-    Graph graph = new Graph();
+    Table tab;
+    Graph graph;
 
     public ViewFrame(){
+        setSize(1000,1000);
         setLayout(new GridLayout(1,2));
+        tab = new Table();
         add(tab);
+        graph = new Graph();
         add(graph);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        setVisible(true);
+        refreshPanels();
+
     }
 
-//    private void refreshPanels(){
-//        timer = new Timer(INTERVAL, new ActionListener() {
-//            public void actionPerformed(ActionEvent evt) {
-//
-//                //Refresh the panel
-//                tab.revalidate();
-//                graph.revalidate();
-//
-//                //if (/* condition to terminate the thread. */) {
-//                //    timer.stop();
-//                //}
-//            }
-//        });
-//
-//        timer.start();
-//
-//
-//    }
+    private void refreshPanels(){
+        timer = new Timer(INTERVAL, new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+                //Refresh the panel
+                tab.update(ploter.getPatternOccurrencesHashMap());
+                tab.revalidate();
+                graph.revalidate();
+
+                //System.out.println("1");
+
+                //if (/* condition to terminate the thread. */) {
+                //    timer.stop();
+                //}
+            }
+        });
+
+        timer.start();
+
+
+    }
 
     public void setPloter(Ploter ploter) {
         this.ploter = ploter;
