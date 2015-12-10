b1eeb48e343f24673cc325162601c8b4a0d8fbbe
kostekman <kostekman1@gmail.com>
2015-12-10 16:39:50 +0100

    Update ParserFile.java

--- src/main/java/pl/edu/agh/ki/to2/parser/parsingControl/ParserFile.java
+++ src/main/java/pl/edu/agh/ki/to2/parser/parsingControl/ParserFile.java
@@ -5,22 +5,23 @@ import main.java.pl.edu.agh.ki.to2.parser.exceptions.UnsupportedFileException;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import org.apache.commons.io.FilenameUtils;
 
 public class ParserFile{
 
     private File file;
     private URL url;
     private int depth;
+    private String fileExtension;
     
     /*list of all supported files*/
 	private static final ArrayList<String> supportedFiles = new ArrayList<String>() {{
         add("html");
-        add("doc");
-        add("pdf");
     }};
 
     public ParserFile(File file, URL url, int depth)  throws UnsupportedFileException {
-        if(supportedFiles.contains(this.getFileExtension())) {
+    	fileExtension = getExtension(file.getName());
+        if(supportedFiles.contains(this.fileExtension)) {
             this.file = file;
             this.url = url;
             this.depth = depth;
@@ -30,14 +31,14 @@ public class ParserFile{
         }
     }
 
-    public String getFileExtension() {
-        String name = this.file.getName();
-        try {
-            return name.substring(name.lastIndexOf(".") + 1);
-        } catch (Exception e) {
-            return "";
-        }
-    }
+    //public String getFileExtension() {
+    //    String name = this.file.getName();
+    //    try {
+    //        return name.substring(name.lastIndexOf(".") + 1);
+    //    } catch (Exception e) {
+    //        return "";
+    //    }
+    //}
     
     public File getFile() {
         return file;
