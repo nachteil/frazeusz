3c11f2b5a20250aa7484a2267bbadb4f186786ed
Rafal <robbie16@vp.pl>
2015-12-13 19:01:19 +0100

    plotter initialization added

--- src/main/java/pl/edu/agh/ki/to2/crawler/Main.java
+++ src/main/java/pl/edu/agh/ki/to2/crawler/Main.java
@@ -8,6 +8,8 @@ import pl.edu.agh.ki.to2.monitor.Monitor;
 import pl.edu.agh.ki.to2.nlprocessor.NLProcessor;
 import pl.edu.agh.ki.to2.parser.Parser;
 import pl.edu.agh.ki.to2.patternmatcher.PatternMatcher;
+import pl.edu.agh.ki.to2.plotter.Ploter;
+import pl.edu.agh.ki.to2.plotter.ViewFrame;
 
 import java.awt.*;
 import java.net.MalformedURLException;
@@ -20,8 +22,11 @@ public class Main {
         NLProcessor nlProcessor = new NLProcessor();
         Monitor monitor = Monitor.getInstance();
         patternMatcher = new PatternMatcher(nlProcessor, monitor.getMonitorPubSub());
-//        Ploter plotter = new Ploter();
-//what with plotter?
+        Ploter ploter = new Ploter();
+        ViewFrame viewFrame = new ViewFrame();
+        ploter.setViewFrame(viewFrame);
+        viewFrame.setPloter(ploter);
+        patternMatcher.addListener(ploter);
         DateFrameController dateFrameController = new DateFrameController();
         EventQueue.invokeLater(() -> new DataFrame(dateFrameController));
     }
