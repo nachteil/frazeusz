90546cff28bb62087b20c9c358a3bfb9df9b50bc
NynaPykulska <nina.pikulska@gmail.com>
2015-12-17 00:38:14 +0100

    Update Graph.java

--- src/main/java/pl/edu/agh/ki/to2/plotter/Graph.java
+++ src/main/java/pl/edu/agh/ki/to2/plotter/Graph.java
@@ -1,9 +1,9 @@
-package pl.edu.agh.ki.to2.plotter;
-
-import javax.swing.*;
+package Pane;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Paint;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -15,16 +15,27 @@ import javax.swing.border.Border;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.DefaultCategoryDataset;
-import pl.edu.agh.ki.to2.patternmatcher.models.SearchPattern;
-import pl.edu.agh.ki.to2.plotter.model.Occurrences;
 
 public class Graph extends JPanel{
 	
 	DefaultCategoryDataset dataset;
 	ChartPanel pane;
 	Border empty;
+	ChartFactory chartFactory;
+	BarRenderer renderer;
+	CategoryPlot plot;
+	Paint color = new Color(0, 172, 178);
 	
 	
 	public Graph(){
@@ -38,15 +49,43 @@ public class Graph extends JPanel{
 		
 		JFreeChart chart = ChartFactory.createBarChart("","","",dataset,PlotOrientation.HORIZONTAL, false, true, false);
 		
+		
+		plot = chart.getCategoryPlot();
+		
+        plot.setNoDataMessage("Brak wyników");
+		plot.setBackgroundPaint(Color.WHITE);
+		plot.setOutlineVisible(false);
+		
+		NumberAxis numberAxis = (NumberAxis)plot.getRangeAxis();
+		numberAxis.setVisible(false);
+		//numberAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		
+	
+		renderer = (BarRenderer) plot.getRenderer();
+		
+		renderer.setGradientPaintTransformer(null);   
+		renderer.setBarPainter(new StandardBarPainter());
+		renderer.setMaximumBarWidth(.35);
+				
+		renderer.setSeriesItemLabelGenerator(0,new StandardCategoryItemLabelGenerator()); 
+		renderer.setSeriesItemLabelsVisible(0, true);
+		
+		
+		//plot.setRenderer(barRenderer);
+        //CategoryItemRenderer
+		
+		
+		
 		pane = new ChartPanel(chart);
 		
 		add(pane);
+		
 		
 	}
 	
 	
 	public void update(Map<SearchPattern,Occurrences> searches){
-		//dataset = null;
+		dataset.clear();
 		SearchPattern key;
 		Map<String, List<String>> map;
 		//String url;
@@ -54,21 +93,19 @@ public class Graph extends JPanel{
 		int counter;
 		for (Map.Entry<SearchPattern, Occurrences> entry : searches.entrySet()) {
 			key = entry.getKey();
-			map = entry.getValue().getUrlSentenceMap();
+			map = entry.getValue().getMap();
 			counter = 0;
 			for (Map.Entry<String, List<String>> lowerEntry : map.entrySet()){
 				//url = lowerEntry.getKey();
 				list = lowerEntry.getValue();
 				counter+=list.size();
 			}
-			dataset.addValue(counter, "" , key.getPattern());
+			dataset.addValue(counter, "" , key.getDescription());
 		}
-		//tab.repaint();
-	}
-	
-	public void editValue (SearchPattern pattern, int value){
+		renderer.setSeriesPaint(0,color);
+		
 		
 	}
 	
+	
 }
-
