ddf51d21c9cd79d97f9f968950ebd51e0a863d39
FreexD <michalwoln@gmail.com>
2016-01-07 23:46:56 +0100

    parser dependencies added

--- src/main/java/pl/edu/agh/ki/to2/parser/parsers/DOCParser.java
+++ src/main/java/pl/edu/agh/ki/to2/parser/parsers/DOCParser.java
@@ -1,8 +1,18 @@
 package pl.edu.agh.ki.to2.parser.parsers;
 
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.nibor.autolink.LinkExtractor;
+import org.nibor.autolink.LinkSpan;
 import pl.edu.agh.ki.to2.parser.parsingControl.ParserFile;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,10 +24,59 @@ public class DOCParser implements IFileParser {
     }
 
     public Set<URL> getUrls(ParserFile parserFile) {
-        return null;
+
+        Set<URL> urls = new HashSet<>();
+
+        InputStream in = null;
+        try {
+            in = parserFile.getUrl().openStream();
+            HWPFDocument wordDoc = new HWPFDocument(in);
+            WordExtractor extractor = new WordExtractor(wordDoc);
+            String str = extractor.getText();
+            LinkExtractor linkExtractor = LinkExtractor.builder().build();
+            Iterable<LinkSpan> links = linkExtractor.extractLinks(str);
+
+            for (LinkSpan link : links) {
+                System.out.println("FOUND URL: " + str.substring(link.getBeginIndex(), link.getEndIndex()));
+                try {
+                    urls.add(new URL(str.substring(link.getBeginIndex(), link.getEndIndex())));
+                } catch (MalformedURLException e) {
+                    // TODO Auto-generated catch block
+                    System.out.println("Caught malformed url!");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return urls;
+
     }
 
     public List<String> getSentences(ParserFile parserFile) {
-        return null;
+
+        List<String> sentences = new ArrayList<String>();
+
+        InputStream in = null;
+        try {
+            in = parserFile.getUrl().openStream();
+            HWPFDocument wordDoc = new HWPFDocument(in);
+            WordExtractor extractor = new WordExtractor(wordDoc);
+            String str = extractor.getText();
+            //System.out.println("DOC FILE TEXT: " + str);
+            for (String sentence : str.split("\\.")) {
+                if(!sentence.trim().isEmpty()) {
+                    sentences.add(sentence);
+                    //System.out.println("SENTENCE TEXT: " + sentence);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return sentences;
     }
 }
