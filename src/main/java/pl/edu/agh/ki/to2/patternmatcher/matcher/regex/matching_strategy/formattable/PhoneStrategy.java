f7f9d43492d30108aea5b950955dd51171728ad3
Quirk0o <beata@obrok.eu>
2016-01-13 20:31:59 +0100

    Implement format method for phone and email strategies

--- src/main/java/pl/edu/agh/ki/to2/patternmatcher/matcher/regex/matching_strategy/formattable/PhoneStrategy.java
+++ src/main/java/pl/edu/agh/ki/to2/patternmatcher/matcher/regex/matching_strategy/formattable/PhoneStrategy.java
@@ -2,19 +2,70 @@ package pl.edu.agh.ki.to2.patternmatcher.matcher.regex.matching_strategy.formatt
 
 import pl.edu.agh.ki.to2.nlprocessor.IWordProvider;
 
+import java.util.Map;
+
 public class PhoneStrategy extends AbstractFormattableStrategy {
 
     private static String phonePattern =
-            "((?:\\+|00)\\d\\d\\u0020?)?" +
-            "((\\d{3}(?<sep1>[ \\-]?)\\d{3}\\k<sep1>\\d{3})|" +
-            "(\\d{2}[ \\-]\\d{3}(?<sep2>[ \\-]?)\\d{2}\\k<sep2>\\d{2}))";
+            "(?:(?:\\+|00)(?<country>\\d\\d)\\u0020?)?" +
+            "(?:(?<num1>\\d{3})" +
+            "(?<sep1>[ \\-]?)" +
+            "(?<num2>\\d{3})" +
+            "\\k<sep1>" +
+            "(?<num3>\\d{3}))|" +
+            "(?:(?<area>\\d{2})" +
+            "[ \\-]" +
+            "(?<anum1>\\d{3})" +
+            "(?<sep2>[ \\-]?)" +
+            "(?<anum2>\\d{2})" +
+            "\\k<sep2>" +
+            "(?<anum3>\\d{2}))";
 
     public PhoneStrategy(IWordProvider wordProvider) {
         super(wordProvider);
     }
 
     @Override
-    protected String getRegex() {
+    protected String getFormatRegex() {
         return phonePattern;
+    }
+
+    @Override
+    protected String[] getGroupNames() {
+        return new String[] { "country", "area", "num1", "num2", "num3", "anum1", "anum2", "anum3" };
+    }
+
+    @Override
+    protected String getReplaceRegex(Map<String, String> groupValues) {
+        if (groupValues.get("area") != null) {
+            return String.format(
+                    "%s" +
+                    "[ \\-]" +
+                    "%s" +
+                    "(?<sep2>[ \\-]?)" +
+                    "%s" +
+                    "\\k<sep2>" +
+                    "%s",
+                    groupValues.get("area"),
+                    groupValues.get("anum1"),
+                    groupValues.get("anum2"),
+                    groupValues.get("anum3"));
+        }
+        else {
+            return String.format(
+                    "(?:\\+|00)" +
+                    "%s" +
+                    "\\u0020?)?" +
+                    "%s" +
+                    "(?<sep1>[ \\-]?)" +
+                    "%s" +
+                    "\\k<sep1>" +
+                    "%s",
+                    groupValues.get("country"),
+                    groupValues.get("num1"),
+                    groupValues.get("num2"),
+                    groupValues.get("num3")
+            );
+        }
     }
 }
